74215ea0b96c531783d74c10dcce1c44658fa17a
Rustam <rustamkholow5@gmail.com>
2026-04-21 15:16:46 +0300

    getStateInstance is ready

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ async function getSettings() {
         document.getElementById('result').value = "Ошибка" + error
     }
 }
+
+async function getStateInstance() {
+    const id = document.getElementById('idInstance').value
+    const token = document.getElementById('apiToken').value
+
+    const url = `https://api.green-api.com/waInstance${id}/getStateInstance/${token}`
+
+    try {
+        const response = await fetch(url)
+        const data = await response.json()
+
+        document.getElementById('result').value = JSON.stringify(data, null, 2)
+    } catch (error) {
+        document.getElementById('result').value = "Ошибка" + error
+    }
+}
